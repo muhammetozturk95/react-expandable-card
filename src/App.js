639bd3c4e64decb5289230613d3301719d1f3e9d
Muhammet Öztürk <mhmozturk7@gmail.com>
2022-10-03 10:54:36 +0300

    expandable-card added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import {motion} from 'framer-motion';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div 
+        layout
+        transition={{layout: {duration:1, type: "spring"}}}
+        onClick={() => setIsOpen(!isOpen)} className='card'
+        style={{borderRadius: "2rem", boxShadow: '0px, 10px, 30px rgba(0, 0, 0, 0.5)'}}>
+
+        <motion.h2>Framer-motion 🚀</motion.h2>
+        {isOpen &&
+        <motion.div
+          className='expand'
+          initial={{opacity: 0}}
+          animate={{opacity:1}}
+          transition={{duration: 1}}
+          >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ex exercitationem aliquid ipsum quae alias, fugiat voluptatum tempore autem maxime?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, debitis!</p>
+      </motion.div>
+        }
+      </motion.div>
     </div>
   );
 }
